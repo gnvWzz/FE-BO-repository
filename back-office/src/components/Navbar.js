beac8ca09863cddb8ad3cfb5f25d8b043e939c2b
TUNGHUYNH <you@example.comhuynhthanhtunga1@gmail.com git config --global user.name Your>
2023-04-04 11:09:53 +0700

    Tung edit FE-BO components at 11h 4/4

--- back-office/src/components/Navbar.js
+++ back-office/src/components/Navbar.js
@@ -3,15 +3,19 @@ import { Input} from 'antd';
 
 const { Search } = Input;
   
-function Navbar() {
+function Navbar({children}) {
 return (
-    <div>
+    <div className='container-fluid m-0'>
       <nav>
         <div style={{ display: "flex", width: "100%" }}>
           <Search placeholder="input search text" enterButton="Search" size="middle" style={{ flex: "auto" }} />
           <div style={{ verticalAlign: "middle", marginLeft: "20px" }}>Profile</div>
         </div>
       </nav>
+
+      <main className='p-0'>
+      {children}
+      </main>
     </div>
     )
 }
