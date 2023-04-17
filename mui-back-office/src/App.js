39a9e42ce1e9985c8198028f2d3c2b23dbcf9659
nguyenhai <nguyenhai190497@gmail.com>
2023-04-17 09:06:54 +0700

    Hải: Chỉnh sửa lỗi ở Component Login.js

--- mui-back-office/src/App.js
+++ mui-back-office/src/App.js
@@ -1,16 +1,12 @@
-// import logo from './logo.svg';
-// <img src={logo} className="App-logo" alt="logo" />
-import SideBar from "./components/SideBar";
+
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes,Route, BrowserRouter } from "react-router-dom";
 import ManufacturerList from "./pages/manufacturer/ManufacturerList";
-import Topbar from "./components/Topbar";
 import ProductList from "./pages/product/ProductList";
 import ManufacturerInfo from "./pages/manufacturer/ManufacturerInfo";
 import ProductInfo from "./pages/product/ProductInfo";
 import AddProduct from "./pages/product/AddProduct";
-// import Account from "./pages/account/Account";
 import AddManufacturer from "./pages/manufacturer/AddManufacturer";
 import Calendar from "./components/Calendar";
 import MemberList from "./pages/member/MemberList";
@@ -19,7 +15,6 @@ import AddMemberImage from "./pages/member/AddMemberImage";
 import AddManufacturerImage from "./pages/manufacturer/AddManufacturerImage";
 import AddProductImages from "./pages/product/AddProductImages";
 import Login from "./components/Login";
-
 import Layout from "./components/Layout";
 import SignUp from "./components/SignUp";
 import Store from "./pages/manufacturer/Store";
@@ -29,50 +24,7 @@ function App() {
   const isLoggedIn = false; // đổi thành true nếu người dùng đã đăng nhập
 
   return (
-// <<<<<<< HEAD
-//     <BrowserRouter>
-//       <ColorModeContext.Provider value={colorMode}>
-//         <ThemeProvider theme={theme}>
-//           <CssBaseline />
-//           <div className="app">
-//             {isLoggedIn && <SideBar />}
-//             <main className="content">
-//               {isLoggedIn && <Topbar />}
-//               <Routes>
-//                 <Route exact path="/" element={<Login />} />
-//                 <Route exact path="/signup" element={<SignUp />} />
-//                 {isLoggedIn && (
-//                   <>
-//                     <Route exact path="/member/list" element={<MemberList />} />
-//                     <Route exact path="/member/add" element={<AddMember />} />
-//                     <Route exact path="/member/:memberId" element={<Account />} />
-//                     <Route
-//                       exact
-//                       path="/member/add-image/:memberId"
-//                       element={<AddMemberImage />}
-//                     />
-//                     <Route
-//                       exact
-//                       path="/manufacturer/add-image/:manufacturerId"
-//                       element={<AddManufacturerImage />}
-//                     />
-//                     <Route exact path="/manufacturer/list" element={<ManufacturerList />} />
-//                     <Route exact path="/manufacturer/:manufacturerId" element={<ManufacturerInfo />} />
-//                     <Route exact path="/manufacturer/add" element={<AddManufacturer />} />
-//                     <Route exact path="/bo/product/list" element={<ProductList />} />
-//                     <Route exact path="/bo/product/:productId" element={<ProductInfo />} />
-//                     <Route exact path="/bo/product/add" element={<AddProduct />} />
-//                     <Route exact path="/account" element={<Account />} />
-//                     <Route exact path="/calendar" element={<Calendar />} />
-//                   </>
-//                 )}
-//               </Routes>
-//             </main>
-//           </div>
-//         </ThemeProvider>
-//       </ColorModeContext.Provider>
-//     </BrowserRouter>
-// =======
+
     <div className="app">
       <main className="content">
     <BrowserRouter>
